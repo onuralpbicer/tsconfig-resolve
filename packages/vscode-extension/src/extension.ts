eafea8b412a9f9a9ead50b705b2ec9deb2932b14
Onur Alp Bicer <onuralpbicer@gmail.com>
2026-03-05 00:00:13 +1100

    Added some libs to keep code organised

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from "vscode";
 import { TSConfigViewProvider } from "./viewProvider";
+import { testing } from "@tsconfig-resolve/core";
 
 export function activate(context: vscode.ExtensionContext) {
     const provider = new TSConfigViewProvider(context.extensionUri);
 
     context.subscriptions.push(
         vscode.window.onDidChangeActiveTextEditor((editor) => {
+            console.log(testing);
             provider.updateDocument(editor?.document);
         }),
     );
