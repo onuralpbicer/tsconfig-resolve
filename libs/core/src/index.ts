9db123738fc217e5a4a871aa32a8ba69b4a5e738
Onur Alp Bicer <onuralpbicer@gmail.com>
2026-03-05 22:53:23 +1100

    WIP: Active ts config resolution

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,1 +1,117 @@
-export const testing = "123";
+import fs from "node:fs/promises";
+import path from "node:path";
+import ts from "typescript";
+
+export async function findNearestTsConfig(
+    filepath: string,
+    workspaceRoot: string,
+) {
+    console.log("filepath", filepath);
+    console.log("workspaceRoot", workspaceRoot);
+
+    const relativeFile = path.relative(workspaceRoot, filepath);
+
+    const rootDir = path.dirname(workspaceRoot);
+    let dir = path.dirname(filepath);
+    let tsConfig = "";
+
+    do {
+        const dirContents = await fs.readdir(dir);
+
+        for (const file of dirContents) {
+            const stat = await fs.stat(path.join(dir, file));
+            console.log("checking", path.join(dir, file));
+
+            if (stat.isDirectory()) continue;
+
+            const candidate = path.join(dir, file);
+            if (
+                file.startsWith("tsconfig") &&
+                file.endsWith(".json") &&
+                (await verifyTsConfigOnFile(relativeFile, candidate))
+            ) {
+                tsConfig = candidate;
+                break;
+            }
+        }
+
+        dir = path.resolve(dir, "..");
+    } while (!tsConfig && rootDir !== dir);
+
+    console.log(
+        "ts package...",
+        ts.findConfigFile(filepath, ts.sys.fileExists),
+    );
+
+    console.log(
+        "ts package...",
+        ts.findConfigFile(filepath, ts.sys.fileExists, "tsconfig.app.json"),
+    );
+
+    console.log(
+        "ts package...",
+        ts.findConfigFile(filepath, ts.sys.fileExists, "tsconfig.spec.json"),
+    );
+
+    console.log(
+        "spec files",
+        (
+            await readTsConfigFile(
+                ts.findConfigFile(
+                    filepath,
+                    ts.sys.fileExists,
+                    "tsconfig.spec.json",
+                )!,
+            )
+        ).fileNames,
+    );
+
+    console.log(
+        "app files",
+        (
+            await readTsConfigFile(
+                ts.findConfigFile(
+                    filepath,
+                    ts.sys.fileExists,
+                    "tsconfig.spec.json",
+                )!,
+            )
+        ).fileNames,
+    );
+
+    console.log(
+        "normal config files",
+        (
+            await readTsConfigFile(
+                ts.findConfigFile(
+                    filepath,
+                    ts.sys.fileExists,
+                    "tsconfig.json",
+                )!,
+            )
+        ).fileNames,
+    );
+
+    return tsConfig;
+}
+
+export async function verifyTsConfigOnFile(
+    relativePath: string,
+    tsConfig: string,
+) {
+    // todo: check if this tsconfig file actually applies to the current file
+    const tsConfigJson = await readTsConfigFile(tsConfig);
+
+    // console.log(tsConfigJson.fileNames); // wow...
+    return false;
+}
+
+export async function readTsConfigFile(tsConfig: string) {
+    const tsConfigDir = path.dirname(tsConfig);
+
+    return ts.parseJsonConfigFileContent(
+        JSON.parse(await fs.readFile(tsConfig, "utf-8")),
+        ts.sys,
+        tsConfigDir,
+    );
+}
--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -1,13 +1,37 @@
 import * as vscode from "vscode";
 import { TSConfigViewProvider } from "./viewProvider";
-import { testing } from "@tsconfig-resolve/core";
+import { findNearestTsConfig } from "@tsconfig-resolve/core";
 
 export function activate(context: vscode.ExtensionContext) {
     const provider = new TSConfigViewProvider(context.extensionUri);
 
     context.subscriptions.push(
-        vscode.window.onDidChangeActiveTextEditor((editor) => {
-            console.log(testing);
+        vscode.window.onDidChangeActiveTextEditor(async (editor) => {
+            const activeEditor = editor?.document.uri;
+            console.log("editor", activeEditor);
+            if (!activeEditor) {
+                // todo: show could not find
+                return;
+            }
+
+            const workspaceRoot =
+                vscode.workspace.getWorkspaceFolder(activeEditor);
+
+            if (!activeEditor || !workspaceRoot) {
+                // todo: show could not find
+                return;
+            }
+
+            const tsConfig = await findNearestTsConfig(
+                editor.document.fileName,
+                workspaceRoot.uri.fsPath,
+            );
+
+            // if (tsConfig) {
+            //     console.log("found tsconfig", tsConfig);
+            // } else {
+            //     console.log("could not find tsconfig");
+            // }
             provider.updateDocument(editor?.document);
         }),
     );
